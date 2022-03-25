0c267d715d808d2a7f755a76fb5a5408a6ddba6b
Rita Rak <rak.rita@gmail.com>
2022-03-25 16:06:51 +0100

    Add project files.

---
+++ Calculator/CalculationMethod.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculationMethod
+    {
+        double nummer1 { get; set; }
+        double nummer2 { get; set; }
+        string counter { get; set; }
+
+        double result;
+
+        int test = 0;
+
+
+
+        public CalculationMethod(double nummer1, double nummer2, string counter)
+        {
+            this.nummer1 = nummer1;
+            this.nummer2 = nummer2;
+            this.counter = counter;
+        
+
+            switch (counter)
+            {
+                case "+":
+                    Add();
+                    break;
+
+                case "-":
+                    Subtract();
+                    break;
+                case "/":
+                    Divide();
+                    break;
+                case "*":
+                    Multiply();
+                    break;
+
+            }
+            Console.WriteLine("\n{0}\t{1}\t{2}\t= {3}", this.nummer1, this.counter, this.nummer2, result);
+        }
+        public double Add()
+        {
+            result = nummer1 + nummer2;
+            return result;
+        }
+
+        public double Subtract()
+        {
+            result = nummer1 - nummer2;
+            return result;
+        }
+        public double Multiply()
+        {
+            result = nummer1 * nummer2;
+            return result;
+        }
+        public double Divide()
+        {
+            while (nummer1 == 0 || nummer2 == 0)
+            {
+                Console.WriteLine("Du försöker dividera med noll");
+
+                if (nummer1 == 0)
+                {
+                    Console.WriteLine("Ange ett nytt första tal");
+                    nummer1 = Convert.ToDouble(Console.ReadLine());
+                }
+
+
+                else if (nummer2 == 0)
+                {
+                    Console.WriteLine("Ange ett nytt andra tal");
+
+
+                    nummer2 = Convert.ToDouble(Console.ReadLine());
+                }
+
+
+            }
+
+            result = nummer1 / nummer2;
+
+
+            return result;
+        }
+
+
+    }
+
+
+
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Calculator;
+using System;
+
+
+
+double nr1;
+double nr2;
+string firstNumStr;
+string op;
+
+
+while (true) { 
+
+
+
+    Console.WriteLine("Welcome to the calculator");
+    Console.WriteLine("Press x to exit");
+
+
+    Console.WriteLine();
+    Console.WriteLine("Skriv in ett nummer, och tryck Ener");
+    firstNumStr = Console.ReadLine();
+    if (firstNumStr == "x") break;
+    nr1=Convert.ToDouble(firstNumStr);
+    
+
+    Console.WriteLine("Skriv in ett annat nummer, och tryck Enter");
+    nr2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Välj ett av följande val");
+    Console.WriteLine("+   addition");
+    Console.WriteLine("-   subraktion");
+    Console.WriteLine("/   division");
+    Console.WriteLine("*   multiplikation");
+ 
+    op = Console.ReadLine();
+
+
+    CalculationMethod m = new CalculationMethod(nr1,nr2, op );
+
+
+
+}
+
+
+Console.WriteLine("GoodBye");
+Console.ReadLine();
+
+double a = 4.5;
+double b = 7.5;
+double c = a + b;
+Console.WriteLine("$hej," +c);
+
